ab3ad415e31da53273b2ab3338fe6cb64637cc7e
g977284333 <977284333@qq.com>
2015-05-19 21:51:19 +0800

    封装的adapter

---
+++ kw_support/src/main/java/com/kw_support/base/KwBaseAdapter.java
@@ -0,0 +1,66 @@
+package com.kw_support.base;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.kw_support.utils.ViewHolder;
+
+public abstract class KwBaseAdapter<T> extends BaseAdapter{
+	protected Context mContext;
+	protected LayoutInflater mInflater;
+	protected List<T> mDatas;
+	protected final int mItemLayoutId;
+	
+	public KwBaseAdapter(Context context, List<T> datas, int itemLayoutId) {
+		this.mContext = context;
+		this.mInflater = LayoutInflater.from(context);
+		this.mDatas = datas;
+		mItemLayoutId = itemLayoutId;
+	}
+	
+	
+	@Override
+	public int getCount() {
+		return mDatas == null ? 0 : mDatas.size();
+	}
+
+	@Override
+	public T getItem(int position) {
+		return mDatas == null ? null : mDatas.get(position);
+	}
+
+	@Override
+	public int getViewTypeCount() {
+		return super.getViewTypeCount();
+	}
+
+	@Override
+	public int getItemViewType(int position) {
+		return super.getItemViewType(position);
+	}
+	
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final ViewHolder viewHolder = getViewHolder(position, convertView, parent);
+		convert(viewHolder, getItem(position));
+		return viewHolder.getConvertView();
+	}
+	
+	public abstract void convert(ViewHolder viewHolder, T item);
+
+	private ViewHolder getViewHolder(int position, View convertView,
+			ViewGroup parent) {
+		return ViewHolder.getViewHolder(mContext, convertView, parent, mItemLayoutId, position);
+	}
+
+}
---
+++ kw_support/src/main/java/com/kw_support/utils/ViewHolder.java
@@ -0,0 +1,78 @@
+package com.kw_support.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewHolder {
+	private final SparseArray<View> mViews;
+	private int mPosition;
+	private View mConvertView;
+	
+	private ViewHolder(Context context, ViewGroup parent, int layoutId, int position) {
+		this.mPosition = position;
+		this.mViews = new SparseArray<View>();
+		this.mConvertView = LayoutInflater.from(context).inflate(layoutId, parent,
+				false);
+		mConvertView.setTag(this);
+	}
+	
+	public static ViewHolder getViewHolder(Context context, View convertView, ViewGroup parent,
+			int layoutId, int position) {
+		if(convertView == null) {
+			return new ViewHolder(context, parent, layoutId, position);
+		}
+		return (ViewHolder) convertView.getTag();
+	}
+	
+	public View getConvertView() {
+		return mConvertView;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T extends View> T getView(int viewId) {
+		View view = mViews.get(viewId);
+		if(view == null) {
+			view = mConvertView.findViewById(viewId);
+			mViews.put(viewId, view);
+		}
+		return (T) view;
+	}
+	
+	public ViewHolder setText(int viewId, String text) {
+		TextView view = getView(viewId);
+		view.setText(text);
+		return this;
+	}
+	
+	public ViewHolder setImageResource(int viewId, int resId) {
+		ImageView view = getView(viewId);
+		view.setImageResource(resId);
+		return this;
+	}
+	
+	public ViewHolder setImageBitmap(int viewId, Bitmap bm) {
+		ImageView view = getView(viewId);
+		view.setImageBitmap(bm);
+		return this;
+	}
+	
+	public int getPostion() {
+		return mPosition;
+	}
+}
+
+
+
+
+
+
+
+
+
+
